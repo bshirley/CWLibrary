380d3efa92808aa7cd88b3cabc515f45f4b1b544
Bill Shirley <bshirley@shirl.com>
2013-11-14 16:19:33 -0600

    initial template files

---
+++ CWLibrary/CWLibrary/CWSecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  CWSecondViewController.h
+//  CWLibrary
+//
+//  Created by Bill Shirley on 11/14/13.
+//  Copyright (c) 2013 shirl.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CWSecondViewController : UIViewController
+
+@end
